0655d47243babdca83b06dcd31f042b6bb6332d5
minhduong200xx <77377938+minhduong200xx@users.noreply.github.com>
2022-12-25 16:23:18 +0700

    Create Header, add files

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
+export { default as CreateContainer } from "./CreateContainer";
+export { default as MainContainer } from "./MainContainer";
